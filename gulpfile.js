678234aa2b05ce56a09a6dee702fa038c19b808d
Erik Bernh <erikb.91@hotmail.com>
2018-04-23 11:45:45 +0200

    Added gulp-useref

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create(); 
+var useref = require('gulp-useref');
+
+// Other requires...
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+var cssnano = require('gulp-cssnano');
+
 
 var projectFolder = 'app';
 var paths = {
@@ -35,4 +43,14 @@ gulp.task('browserSync', function() {
       baseDir: paths.src
     },
   })
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JS file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
 });
